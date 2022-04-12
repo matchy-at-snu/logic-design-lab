a244dc686aaf1db84b5d715a453d54587ba314be
matchy233 <matchy233@gmail.com>
2022-04-12 20:09:19 +0900

    Finish implementing BCD to 7 seg using structural method

--- bcd_to_7_seg_decoder/bcd_to_7_struct.v
+++ bcd_to_7_seg_decoder/bcd_to_7_struct.v
@@ -26,11 +26,52 @@ module bcd_to_7_struct(
 	wire A, B, C, D;
 	wire N_A, N_B, N_C, N_D;
 
+	assign D = NUM[0];
+	assign C = NUM[1];
+	assign B = NUM[2];
+	assign A = NUM[3];
+
 	not(N_D, NUM[0]);
 	not(N_C, NUM[1]);
 	not(N_B, NUM[2]);
 	not(N_A, NUM[3]);
-	
-	nand 
+
+	wire [15:0] M;
+
+	assign M[0] = N_A;
+	nand (M[1], B, D);
+	assign M[2] = N_C;
+	nand (M[3], N_B, N_D);
+	nand (M[4], N_C, N_D);
+	nand (M[5], C, D);
+	assign M[6] = B;
+	assign M[7] = N_B;
+	assign M[8] = C;
+	assign M[9] = N_D;
+	nand (M[10], B, N_C, D);
+	nand (M[11], N_B, C);
+	nand (M[12], B, N_C);
+	nand (M[13], C, N_D);
+
+	// C0
+	nand (SEG[6], M[0], M[1], M[2], M[3]);
+
+	// C1
+	nand (SEG[5], M[4], M[5], M[6]);
+
+	// C2
+	nand (SEG[4], M[7], M[8], M[9]);
+
+	// C3
+	nand (SEG[3], M[0], M[3], M[13], M[10], M[11]);
+
+	// C4
+	nand (SEG[2], M[3], M[13]);
+
+	// C5
+	nand (SEG[1], M[0], M[7], M[4]);
+
+	// C6
+	nand (SEG[0], M[0], M[13], M[11], M[12]);
 
 endmodule
--- bcd_to_7_seg_decoder/test_dataflow.v
+++ bcd_to_7_seg_decoder/test_dataflow.v
@@ -1,50 +1,50 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Company:
+// Company: 
 // Engineer:
 //
-// Create Date:   08:43:01 04/12/2022
-// Design Name:   bcd_to_7_struct
-// Module Name:   /home/ise/_ise_projects/bcd_to_7_seg_decoder/test_struct.v
+// Create Date:   18:41:28 04/12/2022
+// Design Name:   bcd_to_7
+// Module Name:   /csehome/matchy/Documents/logic-design-lab/bcd_to_7_seg_decoder/test_dataflow.v
 // Project Name:  bcd_to_7_seg_decoder
-// Target Device:
-// Tool versions:
-// Description:
+// Target Device:  
+// Tool versions:  
+// Description: 
 //
-// Verilog Test Fixture created by ISE for module: bcd_to_7_struct
+// Verilog Test Fixture created by ISE for module: bcd_to_7_dataflow
 //
 // Dependencies:
-//
+// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-//
+// 
 ////////////////////////////////////////////////////////////////////////////////
 
-module test_struct;
+module test_dataflow;
 
 	// Inputs
 	reg [3:0] NUM;
 
 	// Outputs
 	wire [6:0] SEG;
+	
+	integer i;
 
 	// Instantiate the Unit Under Test (UUT)
-	bcd_to_7_struct uut (
-		.NUM(NUM),
+	bcd_to_7_dataflow uut (
+		.NUM(NUM), 
 		.SEG(SEG)
 	);
 
-	integer i;
-
-	initial begin 
+	initial begin
 		// Initialize Inputs
 		for (i = 0; i < 16; i = i+1) begin
 			NUM = i;
 			#10;
 		end
-
 	end
-
+      
 endmodule
+
