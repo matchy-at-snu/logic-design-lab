a6b786aa550bd1200e55793fc1f6647f58a981bf
matchy233 <matchy233@gmail.com>
2022-04-12 20:14:16 +0900

    Add tempate for BCD-TO-7 behavioral method

---
+++ bcd_to_7_seg_decoder/bcd_to_7_behavior.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:10:22 04/12/2022 
+// Design Name: 
+// Module Name:    bcd_to_7_behavior 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module bcd_to_7_behavior(
+    input [3:0] NUM,
+    output [6:0] SEG
+    );
+	
+	
+
+endmodule
